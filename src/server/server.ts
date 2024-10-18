ce27bdbd3c90314d1f44080f3e0f941792bf66c9
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-18 11:01:57 +0400

    feat: start implementing the app, configure start:dev script

---
+++ src/server/server.ts
@@ -0,0 +1,10 @@
+import http from 'http';
+
+export const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: 'Hello World!',
+    })
+  );
+});
